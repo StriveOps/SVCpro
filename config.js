ea299a36a7da5d56d83e2344d9945cd746bc3004
StriveOps <90435745+StriveOps@users.noreply.github.com>
2026-04-07 11:40:24 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const STRIVE_CONFIG = {
-    firebase: {
-        apiKey: "AIzaSyA0gT_lJAxNDdlYHg7uiU6XUdqSPRgShvs",
-        authDomain: "strive-video-center.firebaseapp.com",
-        databaseURL: "https://strive-video-center-default-rtdb.firebaseio.com",
-        projectId: "strive-video-center"
-    },
-    geminiKey: "AIzaSyA2bUrOy10bhZE2noBikVET09uoylIdxgA"
-};
